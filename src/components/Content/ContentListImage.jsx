350c3e1dcfd65c6125cec23899fb32e034f720d7
VanNDRabiloo <vannd@rabiloo.com>
2022-10-28 20:42:43 +0700

    update homepage link

--- src/components/Content/ContentListImage.jsx
+++ src/components/Content/ContentListImage.jsx
@@ -1,4 +1,4 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 
 function ContentListImage({ data, responsive }) {
   return (
@@ -10,13 +10,13 @@ function ContentListImage({ data, responsive }) {
             data-label={item.label}
             className={`item-image ${responsive}`}
           >
-            <a href="/" className="item-image-link">
+            <a href="/a" className="item-image-link">
               <img src={item.thumbnail} alt="" className="image" />
               <div className="item-title">{item.title}</div>
               <div className="inner-wrapper">
                 <div className="left-inner">
                   <span className="destination">{item.destination}</span>
-                  <p className="desc">{ item?.desc}</p>
+                  <p className="desc">{item?.desc}</p>
                   <div className="rating-wrapper">
                     <i className={item.icon}></i>
                     <p className="rating-text">{item.rating}</p>
